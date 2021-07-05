3b2ea1ab92a2abb71a3afe0bfad2477f75da00f9
Tushar-Rajput21 <86974474+Tushar-Rajput21@users.noreply.github.com>
2021-07-05 21:59:00 +0530

    Add files via upload

---
+++ k-maxsum.cpp
@@ -0,0 +1,34 @@
+// 1005. Maximize Sum Of Array After K Negations 
+
+class Solution {
+public:
+    int largestSumAfterKNegations(vector<int>& nums, int k) {
+        
+       int sum = 0; 
+        
+       sort(nums.begin(), nums.end());
+        
+        int j = k;
+        int l = 0;
+        
+       
+            while(k>0)
+         {
+          nums[0]=-nums[0]; 
+            k--;
+          sort(nums.begin(),nums.end());
+                
+            }
+        
+        
+         for(int i = 0; i < nums.size(); i++)
+            {
+             sum += nums[i];
+          }
+        
+        return sum;
+        
+        
+        
+    }
+};
